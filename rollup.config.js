c8cc2e71e77b7819afb5c13b761a3871e014dc55
Antonio Aguilar <ab6446694@gmail.com>
2018-02-14 20:47:11 +0000

    Release v1.2.0

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,17 @@ import pkg from './package.json';
 export default [
   {
     input: 'src/index.js',
+    output: {
+      name: 'vue-postal',
+      file: pkg.browser,
+      format: 'umd'
+    },
+    plugins: [resolve(), commonjs(), uglify()]
+  },
+  {
+    input: 'src/index.js',
     output: [
+      { file: pkg.main, format: 'cjs' },
       { file: pkg.module, format: 'es' }
     ],
     plugins: [resolve(), commonjs(), uglify()]
